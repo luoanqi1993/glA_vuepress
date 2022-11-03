894fd34a97063a498068cce95f1e82612e9af638
anqi.luo <anqi.luo@ginlong.com>
2022-11-03 16:13:35 +0800

    111

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-cd ./dist
+cd ../dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:luoanqi1993/test2022.github.io.git main
+git push -f git@github.com:luoanqi1993.github.io/test2022.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
