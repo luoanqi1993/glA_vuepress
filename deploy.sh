4757bb2d83d32178065e2aadbb58c24f239a5572
anqi.luo <anqi.luo@ginlong.com>
2022-11-03 16:25:37 +0800

    部署

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,14 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run docs:build
+# 生成静态文件 npm run docs:build
+yarn docs:build
 
 # 进入生成的文件夹
-cd ../dist
+cd ../vueDist/dist
+
+# 将build生成的dist目录拷贝至上一层目录中
+cp -rf docs/.vuepress/dist ../vueDist/
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -15,9 +18,10 @@ cd ../dist
 git init
 git add -A
 git commit -m 'deploy'
+git branch -M main
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:luoanqi1993.github.io/test2022.github.io.git main
+git push -f git@github.com:luoanqi1993/test2022.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // base:'/',
+  base:'/',
   title: 'Hello VuePress',
   description: 'Just playing around',
   themeConfig: {
